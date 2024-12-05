1624acfdc075d776e021fc5a5848a62afd391e93
Matt Girdler <matthewdgirdler@gmail.com>
2024-12-05 12:46:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,10 @@ st.write(
 cnx = st.connection("snowflake") 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
-st.dataframe(data=my_dataframe)
+# st.dataframe(data=my_dataframe)
+# st.stop()
+pd_df = my_dataframe.to_pandas()
+st.dataframe(data=pd_df)
 st.stop()
 
 name_on_order = st.text_input("Name on smoothie")
