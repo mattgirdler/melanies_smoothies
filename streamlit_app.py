9d7c3ecc1cc2937233b5b45070992fc991c34c42
Matt Girdler <matthewdgirdler@gmail.com>
2024-12-05 09:53:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.dataframe(data=smoothiefroot_response, use_container_width=True)
+st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 # st.text(smoothiefroot_response.json())
